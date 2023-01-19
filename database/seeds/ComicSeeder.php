a89fd3e50321bc960e4c7ecf7427a88c66235f12
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-19 20:54:32 +0100

    working progress

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $array_comic = config('comic');
+
+        foreach($array_comic as $elem){
+            $new_comic = new Comic();
+            $new_comic->title = $elem['title'];
+            $new_comic->description = $elem['description'];
+            $new_comic->thumb = $elem['thumb'];            $new_comic->title = $elem['title'];
+            $new_comic->price = $elem['price'];
+            $new_comic->series = $elem['series'];
+            $new_comic->sale_date = $elem['sale_date'];
+            $new_comic->type = $elem['type'];
+            $new_comic->save();
+        }
     }
 }
+ 
